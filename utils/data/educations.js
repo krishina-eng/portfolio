5a4539d9790e82962c3e5e0f08b1911f89542bc5
peaceeng <james.peace.eng@outlook.com>
2024-05-24 10:34:04 -0400

    Initial commit

---
+++ utils/data/educations.js
@@ -0,0 +1,8 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor of Science in Computer Science",
+    duration: "Sep 2011 - Nov 2015",
+    institution: "The Hong Kong University of Science and Technology",
+  },
+];
---
+++ utils/data/experience.js
@@ -0,0 +1,20 @@
+export const experiences = [
+  {
+    id: 1,
+    title: "Senior Software Engineer",
+    company: "Jakroo",
+    duration: "(May 2021 - Present)",
+  },
+  {
+    id: 2,
+    title: "Software Engineer III",
+    company: "Coursera",
+    duration: "(Feb 2017 - Mar 2020)",
+  },
+  {
+    id: 3,
+    title: "Software Engineer",
+    company: "Netradyne",
+    duration: "(Dec 2015 - Jan 2017)",
+  },
+];
